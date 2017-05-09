765325c661f6683f08e47bd6da6ed87199df9766
RainerAtSpirit <R.Wittmann@spirit.de>
2017-05-09 17:14:26 +0200

    updating code to make debugging easier

--- src/mst/models.ts
+++ src/mst/models.ts
@@ -1,12 +1,12 @@
 import {types} from 'mobx-state-tree'
 import { IModelType } from 'mobx-state-tree/lib/types/complex-types/object'
 
-export const ListModel = types.model({
+export const ListModel = types.model('List', {
 	Id: types.identifier(),
 	Title: types.string
 })
 
-export const ViewModel = types.model({
+export const ViewModel = types.model('View', {
 	Id: types.identifier(),
 	Title: types.string,
 	GridConfigJSON: types.string
