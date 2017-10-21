1f6c34c3fcb619266447b6e21f5388d64b103a40
RainerAtSpirit <R.Wittmann@spirit.de>
2017-10-21 13:45:08 +0200

    working version

--- src/core/Model/Odata.ts
+++ src/core/Model/Odata.ts
@@ -1,48 +1,82 @@
 import {types, getParent, getSnapshot} from 'mobx-state-tree'
 import {IModelType} from 'mobx-state-tree/dist/types/complex-types/object'
-import {IObservableArray, toJS} from 'mobx'
+import {IObservableArray} from 'mobx'
 import {ISnapshottable, IType} from 'mobx-state-tree/dist/types/type'
+
+const op1 = types.union(
+  types.literal('eq'),
+  types.literal('neq'),
+  types.literal('gt'),
+  types.literal('gte'),
+  types.literal('lt'),
+  types.literal('lte'),
+  types.literal('in'),
+  types.literal('ne'),
+  types.literal('contains'),
+  types.literal('doesnotcontain')
+)
+const op2 = types.union(
+  types.literal('endswith'),
+  types.literal('startswith'),
+  types.literal('isnull'),
+  types.literal('null'),
+  types.literal('nnull'),
+  types.literal('isnotnull'),
+  types.literal('isempty'),
+  types.literal('isnotempty')
+)
+
+const logicOp1 = types.literal<'and'>('and')
+const logicOp2 = types.literal<'or'>('or')
 
 export const FilterExpression = types.model('FilterExpression', {
   field: types.string,
-  operator: types.union(
-    types.literal('eq'),
-    types.literal('ne'),
-    types.literal('startswith')
-    // todo: implement other
-  ),
+  operator: types.union(op1, op2),
   value: types.frozen
 })
 
-export type IFilterExpression = typeof FilterExpression.Type
+export type FilterExpressionType = typeof FilterExpression.Type
 
 export type FilterGroupInstance = {
-  logic: string,
-  filters: (FilterGroupInstance | IFilterExpression)[]
+  logic: 'and' | 'or',
+  filters: IObservableArray<({} | FilterGroupInstance | FilterExpressionType)>
 }
-export type FilterGroupModel = IModelType<FilterGroupInstance, FilterGroupInstance>
 
-const late = types.late<any, FilterGroupInstance>(() => FilterGroup)
+export type FilterGroupModel = IModelType<FilterGroupInstance, {}, FilterGroupInstance>
+
+const LateFilterGroup = types.late<any, FilterGroupInstance>(() => FilterGroup)
 
 export const FilterOrGroup = types.union(snapshot => snapshot && 'logic' in snapshot
-    ? late
+    ? LateFilterGroup
     : FilterExpression,
-  FilterExpression, late)
+  FilterExpression, LateFilterGroup)
 
 // TypeScript is'nt smart enough to infer self referencing types -> We are smarter ;)
-export const FilterGroup = types.model({
-  logic: types.optional(types.union(types.literal('and'), types.literal('or')), 'and'),
+// export const FilterGroup: FilterGroupModel = types.model('FilterGroup', {
+//   logic: types.optional(types.union(types.literal('and'), types.literal('or')), 'and'),
+//   filters: types.optional(types.array(FilterOrGroup), [])
+// })
+export const FilterGroup = types.model<FilterGroupInstance>('FilterGroup', {
+  logic: types.optional(types.union(logicOp1, logicOp2), 'and'),
   filters: types.optional(types.array(FilterOrGroup), [])
 })
 
-export type IFilterGroup = typeof FilterGroup.Type
+export type FilterGroupType = typeof FilterGroup.Type
 
 export const SortExpression = types.model('SortExpression', {
   dir: types.union(types.literal('asc'), types.literal('desc')),
   field: types.string
 })
 
-type ISortExpression = typeof SortExpression.Type
+type SortExpressionType = typeof SortExpression.Type
+
+export const odataDefaults = {
+  $expand: [],
+  $filter: {},
+  $orderby: [],
+  $select: [],
+  $top: 30
+}
 
 export const ODataStore = types.model('Odata',
   {
@@ -51,7 +85,7 @@ export const ODataStore = types.model('Odata',
     $orderby: types.optional(types.array(SortExpression), []),
     $select: types.optional(types.array(types.string), []),
     $top: types.optional(types.number, 30),
-    get collection() {
+    get collection () {
       return getParent(this)
     }
   }, {
@@ -62,14 +96,14 @@ export const ODataStore = types.model('Odata',
       this.$expand = options
       return this
     },
-    filter (options?: IFilterGroup) {
+    filter (options?: FilterGroupType) {
       if (!options) {
         return getSnapshot(this.$filter)
       }
       this.$filter = options
       return this
     },
-    orderby (options?: ISortExpression[]) {
+    orderby (options?: SortExpressionType[]) {
       if (!options) {
         return getSnapshot(this.$orderby)
       }
@@ -84,3 +118,5 @@ export const ODataStore = types.model('Odata',
       return this
     }
   })
+
+export type IOdataStore = typeof ODataStore.Type
--- test/OData.ts
+++ test/OData.ts
@@ -1,6 +1,6 @@
-import { test } from 'ava'
-import { getSnapshot, onSnapshot } from 'mobx-state-tree'
-import { ODataStore } from '../src/core/Model/Odata'
+import {test} from 'ava'
+import {getSnapshot, onSnapshot} from 'mobx-state-tree'
+import {ODataStore} from '../src/core/Model/Odata'
 
 const complexFilter = {
   logic: 'and',
@@ -37,7 +37,7 @@ const complexFilter = {
     }
   ]
 }
-const simpleFilter = { logic: 'and', filters: [{field: 'test', operator: 'eq', value: 123}] }
+const simpleFilter = {logic: 'and', filters: [{field: 'test', operator: 'eq', value: 123}]}
 
 // === FACTORY TESTS ===
 test('it should produce default Odata options when called with an empty object', (t) => {
@@ -85,7 +85,6 @@ test('it should take partial Odata options', (t) => {
 test('it should throw when trying to update observables directly', (t) => {
 
   const instance = ODataStore.create()
-
   let history: any[] = []
   onSnapshot(instance, data => history.push(data))
 
@@ -148,12 +147,12 @@ test('it should runtime check filter expression', (t) => {
   onSnapshot(instance, data => history.push(data))
 
   let error = t.throws(() => {
-    instance.filter({ logic: 'and', filters: [{field: 'test', operator: 'invalid', value: 123}] })
+    instance.filter({logic: 'and', filters: [{field: 'test', operator: 'invalid', value: 123}]})
   })
   t.regex(error.message, /Error while converting/)
 
   error = t.throws(() => {
-    instance.filter({ logic: 'invalid', filters: [{field: 'test', operator: 'eq', value: 123}] })
+    instance.filter({logic: 'invalid', filters: [{field: 'test', operator: 'eq', value: 123}]})
   })
   t.regex(error.message, /Error while converting/)
 
